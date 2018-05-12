e72d749aec28d56a25d5a65bb8c1d634d262848b
Junmin Park <avin83@gmail.com>
2018-05-11 23:36:27 -0400

    geolocation support

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 
 describe('generateMessage', () => {
@@ -12,4 +12,18 @@ describe('generateMessage', () => {
   //  expect(message.createAt).toBeA('number');
   //  expect(message).toInclude({from,text});
   })
+});
+
+describe('generateLocationMessage', () => {
+  it('should genetrate correct location object', () => {
+    var from = 'Deb';
+    var latitude = 15;
+    var longitude = 19;
+    var url = 'https://www.google.com/maps?=15,19';
+    var message = generateLocationMessage(from, latitude, longitude);
+
+  //  expect(message.createAt).toBeA('number');
+  //  expect(message).toInclude({from, url});
+  });
+
 });
